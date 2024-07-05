2c42b1e29d56e0958d73228f6224c5d44cc94ee1
Panbap <thamphan1919@gmail.com>
2024-07-05 12:17:36 +0700

    5

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: ` Chị trl câu hỏi của em nhé =)))))`,
+    btnIntro: 'OK',
+    title: 'Chị yêu công ty nhiều hôg',
+    desc: 'Chj không trả lời hoặc thoát ra, tức là chj có nhaa :> ',
+    btnYes: 'Uh ok 💖💖💖',
+    btnNo: 'Không ! ',
+    question:'Nói cho em biết lý do chị yêu công ty nhiều như v đi =))) 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửii ! 💌💌',
+    reply: 'chị Yêu công ty nhiều lắmm',
+    mess: 'Em biết mà 🥰. Love chị yêu xiền đúng k 😘😘',
+    messDesc: '💬💬 Nhắn tin cho em nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/quocdat.phan.12'
+}
